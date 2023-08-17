74724bff461dc70059bd987f2f54c975992c7169
Rasel Sharif <raselsharif9@gmail.com>
2023-08-17 17:34:02 +0600

    added function from chatGPT

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 
+// let sum = 0;
+// let appendCount = 0;
+// function priceCalculate(event) {
+//     const itemName = event.parentNode.parentNode.childNodes[1].innerText;
+
+//     const ol = document.createElement('ol');
+//     ol.innerText = itemName;
+//     ol.classList.add('my-2', 'bg-red-400', 'py-3', 'px-2', 'text-white', 'text-2xl')
+//     const itemsContainer = document.getElementById('product-list');
+
+//     const price = parseInt(event.parentNode.childNodes[1].innerText.split(' ')[1]);
+//     if (appendCount < 5) {
+//         itemsContainer.appendChild(ol);
+//         sum += price;
+
+//         const total = document.getElementById('total');
+//         total.innerText = sum;
+//         const totalInNumber = parseInt(total.innerText);
+//         const discount = (30 * totalInNumber) / 100;
+
+//         const discountElement = document.getElementById('discount');
+//         discountElement.innerText = discount;
+
+//         const grandTotal = sum - discount;
+
+//         const grand = document.getElementById('grand');
+//         grand.innerText = grandTotal;
+//         appendCount++
+//     } else {
+//         alert("Limit reached 5! ")
+//     }
+// }
+
 let sum = 0;
 let appendCount = 0;
+const itemLimits = {}; // Object to track item counts
+
 function priceCalculate(event) {
     const itemName = event.parentNode.parentNode.childNodes[1].innerText;
 
-    const ol = document.createElement('ol');
-    ol.innerText = itemName;
-    ol.classList.add('my-2', 'bg-red-400', 'py-3', 'px-2', 'text-white', 'text-2xl')
-    const itemsContainer = document.getElementById('product-list');
+    // Check if item limit is reached
+    if (itemLimits[itemName] === undefined || itemLimits[itemName] < 5) {
+        const ol = document.createElement('ol');
+        ol.innerText = itemName;
+        ol.classList.add('my-2', 'bg-red-400', 'py-3', 'px-2', 'text-white', 'text-2xl')
+        const itemsContainer = document.getElementById('product-list');
 
-    const price = parseInt(event.parentNode.childNodes[1].innerText.split(' ')[1]);
-    if (appendCount < 5) {
+        const price = parseInt(event.parentNode.childNodes[1].innerText.split(' ')[1]);
+
         itemsContainer.appendChild(ol);
         sum += price;
 
@@ -26,8 +63,16 @@ function priceCalculate(event) {
 
         const grand = document.getElementById('grand');
         grand.innerText = grandTotal;
-        appendCount++
+
+        // Update the item count in the object
+        if (itemLimits[itemName] === undefined) {
+            itemLimits[itemName] = 1;
+        } else {
+            itemLimits[itemName]++;
+        }
+
+        appendCount++;
     } else {
-        alert("Limit reached 5! ")
+        alert(`You can't buy more than 5 ${itemName}`);
     }
 }
